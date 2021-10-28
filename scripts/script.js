a8b070a507467568cd6995cda02c10656d5d9369
adv68 <59737974+adv68@users.noreply.github.com>
2021-10-27 23:24:31 -0400

    Worked on starter template
    Mobile first styling
    Started on menus, with dropdowns on desktop on hamburger expanding on mobile

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+function toggleMobileNav() {
+    var nav = document.getElementById("navList");
+    if (nav.style.display === "block") {
+        nav.style.display = "none";
+    } else {
+        nav.style.display = "block";
+    }
+}
