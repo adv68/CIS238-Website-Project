b768eb02d43dec5f8b92409f5892fab92d41c743
adv68 <59737974+adv68@users.noreply.github.com>
2021-11-01 10:12:00 -0400

    moved html content to div wrapper to possibly support overflow-x on samsung browser

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,11 @@
 function closeSideNav(){
 	document.getElementById("sideNav").style.width = "0px";
-	document.getElementById("mainBody").style.marginLeft = "0px";
+	//document.getElementById("mainBody").style.marginLeft = "0px";
+	document.getElementById("contentWrapper").style.marginLeft = "0px";
 }
 
 function openSideNav(){
 	document.getElementById("sideNav").style.width = "250px";
-	document.getElementById("mainBody").style.marginLeft = "250px";
+	//document.getElementById("mainBody").style.marginLeft = "250px";
+	document.getElementById("contentWrapper").style.marginLeft = "250px";
 }
