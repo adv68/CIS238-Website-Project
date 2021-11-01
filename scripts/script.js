a2c78c0646d7f24317313be849969a2817aa6319
adv68 <59737974+adv68@users.noreply.github.com>
2021-11-01 11:24:11 -0400

    TODO still need to make screen slide support for samsung browser

--- scripts/script.js
+++ scripts/script.js
@@ -2,14 +2,16 @@ function closeSideNav(){
 	document.getElementById("sideNav").style.width = "0px";
 	//document.getElementById("mainBody").style.marginLeft = "0px";
 	//document.getElementById("contentWrapper").style.marginLeft = "0px";
-	document.getElementById("contentWrapper").style.transform = "translate(0px, 0px)";
-	//document.getElementById("contentWrapper").style.margin = "0px";
+	//var contentWrapper = document.getElementById("contentWrapper");
+	//contentWrapper.style.transform = "translate(0px, 0px)";
+	
+	document.getElementById("contentWrapper").style.margin = "0px";
 }
 
 function openSideNav(){
 	document.getElementById("sideNav").style.width = "250px";
 	//document.getElementById("mainBody").style.marginLeft = "250px";
 	//document.getElementById("contentWrapper").style.marginLeft = "250px";
-	document.getElementById("contentWrapper").style.transform = "translate(250px, 0px)";
-	//document.getElementById("contentWrapper").style.margin = "0 0 0 250px";
+	//document.getElementById("contentWrapper").style.transform = "translate(250px, 0px)";
+	document.getElementById("contentWrapper").style.margin = "0 0 0 250px";
 }
