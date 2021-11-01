f8cc85d6603c9cb82ac6a5fc497cafe7f4d939e6
adv68 <59737974+adv68@users.noreply.github.com>
2021-11-01 10:03:04 -0400

    modified menu and body for sliding effect
    need to see if compatible with samsung browser

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,9 @@
 function closeSideNav(){
 	document.getElementById("sideNav").style.width = "0px";
+	document.getElementById("mainBody").style.marginLeft = "0px";
 }
 
 function openSideNav(){
 	document.getElementById("sideNav").style.width = "250px";
+	document.getElementById("mainBody").style.marginLeft = "250px";
 }
