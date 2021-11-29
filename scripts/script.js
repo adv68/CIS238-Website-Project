3e0aa79612e9dbcbfa6a39b413dafabf047ae8de
ZephyrsWhisper <hardyza@mail.gvsu.edu>
2021-11-28 22:39:44 -0500

    Major Update to catering.html and friends
    Added script to create a pseudo-cart for aesthetic purposes. Added and changed some style rules pertaining to catering.html mainly. Added two functions that run the cart for catering page.

--- scripts/script.js
+++ scripts/script.js
@@ -18,3 +18,48 @@ function openSideNav(){
 	//document.getElementById("contentWrapper").style.marginLeft = "250px";
 	//document.getElementById("contentWrapper").style.transform = "translate(250px, 0px)";
 }
+
+
+var orders = new Array();
+var qtyOfOrders = {
+	1:[0, "Strawberry Banana"],
+	2:[0, "Wildberry"],
+	3:[0, "Peanut Butter Banana"],
+	4:[0, "Mango"],
+	5:[0, "Greek Salad"],
+	6:[0, "Sandwhiches"],
+	}
+
+function addToCateringCart(){
+	orders = ["<span id='orders-title'>Orders</span>"]
+	
+	var qty = document.getElementById("catering-qty").value;
+	if(isNaN(qty) || qty === ""){
+		return;
+	}
+	
+	var foodID = document.getElementById("catering-dropdown").value;
+	qtyOfOrders[foodID][0] = parseInt(qtyOfOrders[foodID][0]) + parseInt(qty);
+	
+	for(var i = 1; i < Object.keys(qtyOfOrders).length; i++){
+		if(qtyOfOrders[i][0] > 0){
+			var order = "<p>" + qtyOfOrders[i][0] + "x " + qtyOfOrders[i][1] + "</p>";
+			orders.push(order);
+		}
+	}
+	
+	var cart = document.getElementById("catering-cart");
+	var str = "";
+	for(var i = 0; i < orders.length; i++){
+		str += orders[i];
+	}
+	
+	cart.innerHTML = str;
+}
+
+function resetCart(){
+	for(var i = 1; i < Object.keys(qtyOfOrders).length; i++){
+		qtyOfOrders[i][0] = 0
+	}
+	document.getElementById("catering-cart").innerHTML = "<span id='orders-title'>Orders</span>"
+}
